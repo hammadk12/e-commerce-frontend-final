1264aa173f8f6a1a3a869ff4af63f878acec5073
hammadk12 <131798537+hammadk12@users.noreply.github.com>
2024-03-04 23:11:57 -0500

    ADDED SLIDER LOGIC - in Slider.tsx: - added next and previous slide logic - implemented useState with current & setCurrent variables

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { useState } from 'react'
 import Image from 'next/image';
 
 type Slide = {
@@ -10,18 +10,35 @@ type SliderProps = {
 };
 
 const Slider: React.FC<SliderProps> = ({ slides }) => {
+const [current, setCurrent] = useState(0)
+const length = slides.length
+
+const nextSlide = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1)
+}
+
+const prevSlide = () => {
+    setCurrent(current === 0 ?  length - 1 : current - 1)
+}
+
+if(!Array.isArray(slides) || slides.length <= 0) {
+    return null;
+}
+
   return (
     <div id='gallery'>
         <h1>Gallery</h1>
         <div> 
             {slides.map((slide, index) => (
-              <Image 
-                key={index} 
+              <div key={index} className=''>
+              <Image  
                 src={slide.image} 
                 alt={`Slide ${index}`} 
-                width='1440' height='600' 
+                width='740' height='60' 
                 objectFit='cover'
+                className='flex justify-center m-auto'
                 />
+               </div>
             ))}
         </div>
     </div>
