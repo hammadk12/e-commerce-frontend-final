1ab2b8f275479624b70bad2a129b9680b9147bb9
hammadk12 <131798537+hammadk12@users.noreply.github.com>
2024-03-07 21:45:46 -0500

    BEGAN CONTACT PAGE COMPONENT - In Instagram.tsx: - imported necessary images & Instagram.img - set up basic heading and structure - In Instagramimg.tsx: - created Image component with styling - implemented overlay

---
+++ components/Instagram.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import photo1 from '../public/photo1.avif'
+import photo2 from '../public/photo2.avif'
+import photo3 from '../public/photo3.avif'
+import photo4 from '../public/photo4.avif'
+
+
+const Instagram = () => {
+  return (
+    <div>Instagram</div>
+  )
+}
+
+export default Instagram
