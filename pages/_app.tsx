32e74b711a30776e230bfd9a7aff97eaaa307330
hammadk12 <131798537+hammadk12@users.noreply.github.com>
2024-03-31 18:54:25 -0400

    HANDLING TOKEN EXPIRY - in _app.tsx: - set up axios interceptor that runs when application starts - interceptor checks for 401 status, if detected then redirects to login page - interceptor ejects when component unmounts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,41 @@
 // This file is for what is on every page in the application
 import Footer from '../components/Footer';
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import Navbar from '../components/Navbar';
 import '../styles/globals.css';
 import { ScrollProvider } from '../components/ScrollContext';
 
 // Renders Header, Footer, and index.tsx components
 const MyApp: React.FC<{ Component: React.ComponentType, pageProps: any }> = ({ Component, pageProps }) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Set up an Axios interceptor
+    const responseInterceptor = axios.interceptors.response.use(response => {
+        // This function handles all successful responses globally
+        return response;
+    }, error => {
+        // This function handles errors from all responses globally
+        if (error.response && error.response.status === 401) {
+            // If the error status is 401 (Unauthorized)
+
+            // Clear the cookie named 'token'
+            document.cookie = 'token=; Max-Age=0; path=/';
+
+            // Redirect to the login page
+            router.push('/login');
+        }
+        return Promise.reject(error);
+    });
+
+  return () => {
+    // Clean up the interceptor when the component unmounts to prevent memory leaks
+    axios.interceptors.response.eject(responseInterceptor);
+  };
+}, []);
+  
   return (
     <ScrollProvider>
     <div className='flex flex-col min-h-screen'>
